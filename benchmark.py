8705da4952de18c615d8a3fda12f2a97944ce59b
amatt13 <anders.matthiassen@gmail.com>
2018-03-01 14:27:59 +0100

    Windows fix

--- benchmark.py
+++ benchmark.py
@@ -1,28 +1,29 @@
 import subprocess
 import argparse
 import os
-import sys
 import datetime
 
 my_location = os.path.dirname(os.path.realpath(__file__)) + '/'
 linux = 'linux'
 
+
 def linux_machine(OS_name: str):
     return OS_name == linux
 
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Benchmark tool for UPPAAL models')
-    parser.add_argument('-o',  type=str, required=False, help='OS name')
-    parser.add_argument('-i',  type=int, required=False, help='Number of iterations to run', default=1)
+    parser.add_argument('-o', type=str, required=False, help='OS name')
+    parser.add_argument('-i', type=int, required=False, help='Number of iterations to run', default=1)
     parser.add_argument('--write', action='store_true', required=False, help='Write results to a file (set --t to get a trace)')
     parser.add_argument('--t', action='store_true', required=False, help='Generate and show some trace')
     args = parser.parse_args()
-    
+
     OS = args.o
     if args.o is None:
         OS = linux
     write = args.write
-    t = '-X' # -X is not a valid argument and is therefore ignored
+    t = '-X'  # -X is not a valid argument and is therefore ignored
     y = '-X'
     if args.t:
         t = "-t0"
@@ -37,22 +38,16 @@ if __name__ == '__main__':
             output = None
             if write:
                 output = trace_file
-            
+
             start = datetime.datetime.now()
             if linux_machine(OS):
-                subprocess.Popen([my_location + "verifyta", my_location + 
-                    "models/classic_v1.xml", my_location + 
-                    "models/classic.q", 
+                subprocess.Popen([my_location + "verifyta", my_location + "models/classic_v1.xml", my_location + "models/classic.q",
                     "-o1", t, y], stdout=output, stderr=output).wait()
             else:
-                subprocess.Popen([my_location_windows + "verifyta.exe", my_location_windows + 
-                    "models\\classic_v1.xml", my_location_windows + 
-                    "models\\classic.q", 
-                    "-o1", t, y], stdout=output, stderr=output).wait()
+                subprocess.Popen(my_location_windows + "verifyta.exe -o1 " + t + " " + y  + " " + my_location_windows + "models\\classic_v1.xml " + my_location_windows + "models\\classic.q", stdout=output, stderr=output).wait()
             end = datetime.datetime.now()
             time = end - start
             total_seconds = time.total_seconds()
             time_measurements.append(total_seconds)
             print(str(i) + ": " + str(time_measurements[-1]))
     print("Avergae time: " + str(sum(time_measurements) / len(time_measurements)))
-    
