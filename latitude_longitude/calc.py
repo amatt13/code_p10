c9f57537fab8f52b1f6f7bdb7ffeb42beffd0128
amatt13 <anders.matthiassen@gmail.com>
2018-03-08 11:39:53 +0100

    Mindre hæselig kode

--- latitude_longitude/calc.py
+++ latitude_longitude/calc.py
@@ -1,19 +1,22 @@
-# import epoch
-# from numpy import radians
-# from orbit_predictor.predictors import tle
-# from scipy.constants import kilo
-# from orbital import earth_sidereal_day, earth, KeplerianElements, plot, Maneuver, plot3d
-# import matplotlib.pyplot as plt
-# plt.ion()
 import ephem
 import datetime
-# import julian as jul
-
-# from IPython.display import HTML
-# HTML('<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/require.js/2.1.10/require.min.js"></script>')
-
 import numpy as np
 from math import atan2
+
+RADIUS_EARTH = 6.37 * 10 ** 6
+# Newton's Law - universal gravitation constant
+G = 6.673 * 10 ** -11
+
+
+def checksum(line: str):
+    checksum = 0
+    for x in line:
+        if x.isdigit() and int(x) != 0:
+            checksum = checksum + int(x)
+        elif x == "-":
+            checksum = checksum + 1
+    return line + str(checksum)[-1]
+
 
 # line 1
 # 1     - Line Number of Element Data
@@ -30,8 +33,7 @@ from math import atan2
 # 63    - Ephemeris type
 # 65-68 - Element number
 # 69    - Checksum
-def constructLineOne(year, days):
-    b = " "
+def construct_line_one(year, days):
     c1 = "1"
     c3_7 = "99999"
     c8 = " "
@@ -55,14 +57,7 @@ def constructLineOne(year, days):
     line1 = "{0} {1}{2} {3}{4}{5} {6}{7} {8} {9}  {10}  {11}  {12}".format(c1, c3_7, c8, c10_11, c12_14, c15_17, c19_20,
                                                                            c21_32,
                                                                            c34_43, c45_52, c54_61, c63, c65_68)
-    checksum = 0
-    for x in line1:
-        if x.isdigit() and int(x) != 0:
-            checksum = checksum + int(x)
-        elif x == "-":
-            checksum = checksum + 1
-    line1 = line1 + str(checksum)[-1]
-    return line1
+    return checksum(line1)
 
 
 # line 2
@@ -76,7 +71,7 @@ def constructLineOne(year, days):
 # 53-63 - Mean Motion [Revs per day]
 # 64-68 - Revolution number at epoch [Revs]
 # 69    - Checksum
-def constructLineTwo(inclination, RAAN, eccentricity, aPerigee, mAnomaly, mMotion, revolutionAtEpoch):
+def construct_line_two(inclination, RAAN, eccentricity, aPerigee, mAnomaly, mMotion, revolutionAtEpoch):
     b = " "
     c1 = "2"
     c3_7 = "99999"
@@ -89,25 +84,13 @@ def constructLineTwo(inclination, RAAN, eccentricity, aPerigee, mAnomaly, mMotio
     c64_68 = str(revolutionAtEpoch)
     line2 = "{0} {1}  {2}  {3} {4}  {5}  {6} {7}{8}".format(c1, c3_7, c9_16, c18_25, c27_33, c35_42, c44_51, c53_63,
                                                             c64_68)
-    checksum = 0
-    for x in line2:
-        if x.isdigit() and int(x) != 0:
-            checksum = checksum + int(x)
-        elif x == "-":
-            checksum = checksum + 1
-    line2 = line2 + str(checksum)[-1]
-    return line2
+    return checksum(line2)
 
 
-def calculateOrbit(heightAmount):
-    # m
-    radius_earth = 6.37 * 10 ** 6
-    height = heightAmount
-    radius_orbit = radius_earth + height
+def calculate_orbit(height_amount):
+    radius_orbit = RADIUS_EARTH + height_amount
     # kg
     mass_earth = 5.98 * 10 ** 24
-    # Newton's Law - universal gravitation constant
-    G = 6.673 * 10 ** -11
     # Orbital Speed Equations
     v = np.math.sqrt((G * mass_earth) / radius_orbit)
     # Acceleration Equation
@@ -116,8 +99,8 @@ def calculateOrbit(heightAmount):
     t = np.math.sqrt((4 * (np.math.pi ** 2) * (radius_orbit ** 3)) / (G * mass_earth))
     print("Base Properties:")
     print("Universal Gravitation Constant", G)
-    print("Earth's Radius", radius_earth)
-    print("Orbital Height", height)
+    print("Earth's Radius", RADIUS_EARTH)
+    print("Orbital Height", height_amount)
     print("Orbital Radius", radius_orbit)
     print("Earth's mass", mass_earth)
 
@@ -128,44 +111,39 @@ def calculateOrbit(heightAmount):
     return [v, a, t / 60]
 
 
-def generateData(scheduleLength):
-    line1 = line2 = ""
+def generate_data(schedule_length):
     LL = []
     date = datetime.datetime(2018, 3, 7, 9, 40, 39)
     print(date)
-    lat = long = 0
-    for x in range(0, scheduleLength, 1):
+    for x in range(0, schedule_length, 1):
         day_of_year = date.timetuple().tm_yday + (date.hour + ((date.minute + x) / 60)) / 24
-        dateOnly = str(date).split(" ")[0].replace("-", "/")
-        dateYear = str(date).split("-")[0]
-        line1 = constructLineOne(str(dateYear)[2:], day_of_year)
-        line2 = constructLineTwo("50.0000", "020.519", "0020247", "81.2115", "279.186", "16.30050059", "00000")
+        date_only = str(date).split(" ")[0].replace("-", "/")
+        date_year = str(date).split("-")[0]
+        line1 = construct_line_one(str(date_year)[2:], day_of_year)
+        line2 = construct_line_two("50.0000", "020.519", "0020247", "81.2115", "279.186", "16.30050059", "00000")
         tle = ephem.readtle("OrbitName", line1, line2)
-        tle.compute(dateOnly)
+        tle.compute(date_only)
         lat = float(str(tle.sublat).split(":")[-3]) + float(str(tle.sublat).split(":")[-2]) / 60 + float(
             str(tle.sublat).split(":")[-1]) / (60 * 60)
         long = float(str(tle.sublong).split(":")[-3])
         if long < 0:
-            long = long * -1
-            long = long + float(str(tle.sublong).split(":")[-2]) / 60 + float(str(tle.sublong).split(":")[-1]) / (
-                60 * 60)
-            long = long * -1
+            long += (float(str(tle.sublong).split(":")[-2]) / 60 + float(str(tle.sublong).split(":")[-1]) / (
+                60 * 60)) * -1
         else:
-            long = long + float(str(tle.sublong).split(":")[-2]) / 60 + float(str(tle.sublong).split(":")[-1]) / (
+            long += float(str(tle.sublong).split(":")[-2]) / 60 + float(str(tle.sublong).split(":")[-1]) / (
                 60 * 60)
         LL.append([long, lat])
         # print(long, ", ", lat)
         # print('%s %s' % (tle.sublong, tle.sublat))
         # print(str(tle.sublong).split(":")[-3] + "." + str(tle.sublong).split(":")[-2], ", ", str(tle.sublat).split(":")[-3] + "." + str(tle.sublat).split(":")[-2])  # , ", Hours:" + str(x/60))
     print(LL)
-    Stations = [[-45.10, 50.00]]
+    stations = [[-45.10, 50.00]]
     LLxStations = []
-    radius_earth = 6.37 * 10 ** 6
     data_string = 'const bool TWINDOW[SCHEDULE_LENGHT][STATIONS] = {'
     for x in LL:
-        tmpSet = []
+        tmp_set = []
         data_string += '{'
-        for y in Stations:
+        for y in stations:
             lat1 = np.radians(y[0])
             lon1 = np.radians(y[1])
             lat2 = np.radians(x[0])
@@ -177,18 +155,19 @@ def generateData(scheduleLength):
             a = np.sin(dlat / 2) ** 2 + np.cos(lat1) * np.cos(lat2) * np.sin(dlon / 2) ** 2
             c = 2 * atan2(np.sqrt(a), np.sqrt(1 - a))
 
-            distance = radius_earth * c
+            distance = RADIUS_EARTH * c
             print(distance)
             if distance >= 50000:
-                tmpSet.append(0)
+                tmp_set.append(0)
                 data_string += '0,'
             else:
-                tmpSet.append(1)
+                tmp_set.append(1)
                 data_string += '1,'
         data_string = data_string[:-1]
         data_string += '},'
-        LLxStations.append([y[0], y[1], tmpSet])
+        LLxStations.append([y[0], y[1], tmp_set])
     return data_string[:-1] + '};'
 
+
 if __name__ == '__main__':
-    print(generateData(360))
+    print(generate_data(360))
